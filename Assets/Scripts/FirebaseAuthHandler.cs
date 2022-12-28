9122d5e0dbcf9b91be87db2608919cb29ae593bb
Inoussa Ouedraogo <inoussa.ouedraogo@universite-paris-saclay.fr>
2022-12-28 12:53:20 +0100

    Projet Prisca

---
+++ Assets/Scripts/FirebaseAuthHandler.cs
@@ -0,0 +1,26 @@
+﻿using Proyecto26;
+using UnityEngine;
+
+/// <summary>
+/// Handles authentication calls to Firebase
+/// </summary>
+
+public static class FirebaseAuthHandler
+{
+    private const string ApiKey = "AIzaSyC2URgqyjWqQWdpjpFCl8yw9CaAztE8Tak"; //firebase authentification
+
+    /// <summary>
+    /// cote l'utilisateur sur la base de donnée avec son Token
+    /// </summary>
+    public static void SingInWithToken(string token, string providerId)
+    {
+        var payLoad =
+            $"{{\"postBody\":\"id_token={token}&providerId={providerId}\",\"requestUri\":\"http://localhost\",\"returnIdpCredential\":true,\"returnSecureToken\":true}}";
+        RestClient.Post($"https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key={ApiKey}", payLoad).Then(
+            response =>
+            {
+                // You now have the userId (localId) and the idToken of the user!
+                Debug.Log(response.Text);
+            }).Catch(Debug.Log);    
+    }
+}
---
+++ Assets/Scripts/GoogleAuthenticator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Proyecto26;
+using UnityEngine;
+
+
+
+public static class GoogleAuthenticator
+{
+    private const string ClientId = "710485781883-9pnur7v8i7h7gevp02r1cg07avcie0p5.apps.googleusercontent.com"; //ton clien google
+    private const string ClientSecret = "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"; //le passwork
+
+    private const int Port = 1234;//le port va ecouter les reponse de connection
+    private static readonly string RedirectUri = $"http://localhost:{Port}";//ip ou sera rediriger les reponse de connection
+
+    private static readonly HttpCodeListener codeListener = new HttpCodeListener(Port);
+
+    
+    public static void GetAuthCode()
+    {
+        Application.OpenURL($"https://accounts.google.com/o/oauth2/v2/auth?client_id={ClientId}&redirect_uri={RedirectUri}&response_type=code&scope=email");
+
+        codeListener.StartListening(code =>
+        {
+            ExchangeAuthCodeWithIdToken(code, idToken =>
+            {
+                FirebaseAuthHandler.SingInWithToken(idToken, "google.com");
+            });
+            
+            codeListener.StopListening();
+        });
+    }
+    
+    /// <summary>
+    /// echanger le cod identification avec un token
+  
+    public static void ExchangeAuthCodeWithIdToken(string code, Action<string> callback)
+    {
+        try
+        {
+            RestClient.Request(new RequestHelper
+            {
+                Method = "POST",
+                Uri = "https://oauth2.googleapis.com/token",
+                Params = new Dictionary<string, string>
+                {
+                    {"code", code},
+                    {"client_id", ClientId},
+                    {"client_secret", ClientSecret},
+                    {"redirect_uri", RedirectUri},
+                    {"grant_type", "authorization_code"}
+                }
+            }).Then(
+                response =>
+                {
+                    var data =
+                        StringSerializationAPI.Deserialize(typeof(GoogleIdTokenResponse), response.Text) as
+                            GoogleIdTokenResponse;
+                    callback(data.id_token);
+                }).Catch(Debug.Log);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
